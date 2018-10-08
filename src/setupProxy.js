ddd36d8fbceb518671e46673b0f54a898c08c1e7
MAKOSCAFEE <barnabasmakonda@gmail.com>
2018-10-08 13:05:30 +0300

    initial commit

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const proxy = require('http-proxy-middleware');
+
+const localhostProxy = proxy({
+  target: 'http://localhost:8085/',
+  secure: 'false',
+  auth: 'admin:district',
+  changeOrigin: 'true'
+});
+
+module.exports = function(app) {
+  app.use('/api', localhostProxy);
+};
