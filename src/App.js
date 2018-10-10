0f9cf1ba3ab62a9836277ec84e6019b986eebe4f
bellangowi <isabellangowi@gmail.com>
2018-10-10 15:39:13 +0300

    Adding test configurations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { simpleAction } from './actions/simpleAction'
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+simpleAction = (event) => {
+  this.props.simpleAction();
+ }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+  
+
+       <button onClick={this.simpleAction}>Test redux action</button>
+       <pre>
+          {
+            JSON.stringify(this.props)
+          }
+       </pre>
       </div>
+
+
     );
+
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+ });
+ const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
+
+
+
+ export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import configureStore from './store';
 
+ReactDOM.render(
+ <Provider store={configureStore()}>
+  <App />
+ </Provider>,
+ document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
+
+
