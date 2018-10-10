ec1dbbd4a8819fd4254113a357afa749cac3e9f5
Barnabas Makonda <6409210+MAKOSCAFEE@users.noreply.github.com>
2018-10-10 12:06:57 +0300

    Revert "Add store configuration"

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import { simpleAction } from './actions/simpleAction'
 import logo from './logo.svg';
 import './App.css';
 
-
 class App extends Component {
-
-simpleAction = (event) => {
-  this.props.simpleAction();
- }
   render() {
     return (
       <div className="App">
-  
-
-       <button onClick={this.simpleAction}>Test redux action</button>
-       <pre>
-          {
-            JSON.stringify(this.props)
-          }
-       </pre>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
       </div>
-
-
     );
-
   }
 }
 
-const mapStateToProps = state => ({
-  ...state
- });
- const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(simpleAction())
- })
-
-
-
- export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
