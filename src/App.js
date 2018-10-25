ca02a4282728fd6546f59938fb36cb1ef9d8bcd7
MAKOSCAFEE <barnabasmakonda@gmail.com>
2018-10-25 06:13:02 +0300

    fix: update codestyle and fix tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { startupDataLoad } from './store/actions'
+import { startupDataLoad } from './actions'
 import './App.css'
 
 class App extends Component {
